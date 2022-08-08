dd3ca78ac2bd976cda0e286b94defec579246900
Mythical Forest Collective <Random.Access.Exomemory@gmail.com>
2022-08-08 23:02:25 +0100

    Python bad >:(

--- main.nim
+++ main.nim
@@ -1,6 +1,42 @@
+import strutils
+
 import futhark
+
+
+proc idiomaticRenamer(name, kind, partof: string): string =
+  const prefixes = @["WGPU", "Wgpu", "wgpu"]
+
+  result = name
+
+  for prefix in prefixes:
+    result =
+      if result.startsWith(prefix):
+        result[prefix.len..name.high]
+      else:
+        result
+  #[
+  case result:
+  of "String":
+    result = "WasmInternalString"
+  of "Value":
+    result = "WasmValue"
+  of "Result":
+    result = "WasmResult"
+  of "Limit":
+    result = "WasmLimit"
+  elif result.endsWith "Context":
+    result = "Wasm" & result
+  else: discard
+  ]#
+
+  case kind
+  of "const", "typedef", "enum":
+    discard
+  else:
+    result[0] = result[0].toLowerAscii
 
 importc:
   sysPath "/data/data/com.termux/files/usr/lib/clang/14.0.0/include"
   path "."
+  renameCallback idiomaticRenamer
   "wgpu-native/ffi/wgpu.h"
